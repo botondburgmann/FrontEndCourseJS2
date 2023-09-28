e628c604c22131ea94bdb31f9bcf962859bc9827
Botond Burgmann <k811qk@inf.elte.hu>
2023-09-28 14:16:12 +0200

    Calculator works with positive integers

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function add(numOne, numTWo) {
     return Math.round(numOne + numTWo);
 }
@@ -28,3 +29,78 @@ function operate(numOne, operator, numTWo) {
             throw new Error("Error: operator doesn't exist");
     }
 }
+
+//Function for disabling operator and = buttons.
+function disableButtons() {
+    for (const operator of operators)
+        operator.disabled = true;
+    equal.disabled = true;
+}
+
+
+const numbers = document.getElementsByClassName("number"); 
+const operators = document.getElementsByClassName("operator");
+const equal = document.getElementById("equal");
+
+disableButtons();
+const currentOperation = []; // Array for storing the operation
+let concatedNum = '' // 
+
+
+for (const number of numbers) {
+    number.addEventListener("click", () => {
+        concatedNum = concatedNum.concat(number.innerText) // While the user pressing numbers it gets concatenated (e.g. 1, 12, 123)
+        currentOperation.push(concatedNum) // Push the concatenated number (as string into the array)
+        
+        // If the array of operations has 3 elements, one of which is an operator, enable the = button
+        if(currentOperation.length === 3 && (currentOperation.includes('+') || currentOperation.includes('-') || currentOperation.includes('*') || currentOperation.includes('/'))) // If the array for the operation has 3 
+            equal.disabled = false;
+        
+        // Enable the operator buttons
+        for (const operator of operators)
+            operator.disabled = false   
+    });
+
+}
+
+for (const operator of operators) {
+    operator.addEventListener('click', () => {
+    
+    // Disable operator and = buttons until the user presses a number again
+    disableButtons();
+
+    /*  If the array of operations has 3 at elements, one of which is an operator, 
+        evaluate the first pair of numbers and store the result as the first element of the array 
+    */
+    if ((currentOperation.includes('+') || currentOperation.includes('-') || currentOperation.includes('*') || currentOperation.includes('/')) && currentOperation.length > 3){
+        currentOperation.splice(2,(currentOperation.length-2), Number(currentOperation[currentOperation.length-1]) )
+        const result = operate(currentOperation[0], currentOperation[1], currentOperation[2]);
+        console.log(`Current result is: ${result}`); // Here it just logs it on the console, but we should display it on the page
+        currentOperation.splice(0,currentOperation.length, result)
+    }
+
+    // Otherwise it deletes all the elements so far, and replaces it with the last element after converting (So, e.g.: ['1', '12', '123'] will be [123])
+    else
+        currentOperation.splice(0,currentOperation.length, Number(currentOperation[currentOperation.length-1]) );
+
+    // Then append the operator to the end and reset the variable for concatenating the numbers
+    currentOperation.push(operator.innerText);
+    concatedNum = '';
+
+
+    });
+}
+
+
+// When clicking on the = button, do the calculation, and reset everything
+equal.addEventListener('click', () =>{
+    currentOperation.splice(2,(currentOperation.length-2), Number(currentOperation[currentOperation.length-1]) )
+    const result = operate(currentOperation[0], currentOperation[1], currentOperation[2]);
+    console.log(`Final result is: ${result}`); // Here it also just logs it on the console, but we should display it on the page
+    currentOperation.splice(0,currentOperation.length)
+    concatedNum = '';
+    disableButtons();
+
+})
+
+
